fd2286cb030dfeb3f5371a9d7a1fa98b492dfb28
Jaimerson <jaimersonaraujo@gmail.com>
2014-11-25 21:43:37 -0300

    TROLOLOLO

--- state.h
+++ state.h
@@ -6,28 +6,44 @@
 #include "board.h"
 
 void save_game(Board *board){
-  FILE *game;
+  FILE *game, *tries;
+  int i, j;
 
+  tries = fopen("./tries", "w+");
   game = fopen("./save", "w+");
-  fprintf(game, "%i\n", board->tries);
+  fprintf(tries, "%i\n", board->tries);
+
+  for(i = 0; i < BOARD_SIZE; i++){
+    for(j = 0; j < BOARD_SIZE; j++){
+      fprintf(game, "%i", board->tiles[i][j]);
+    }
+  }
 
   fclose(game);
+  fclose(tries);
 
   //mvprintw(BOARD_SIZE, 2, "Game saved!");
 }
 
 void load_game(Board *board){
-  FILE *game;
+  FILE *game, *tries;
+  Color *tile = &(board->tiles);
 
   game = fopen("./save", "r");
+  tries = fopen("./tries", "r");
 
-  if(game == NULL){
+  if(tries == NULL || tries == NULL){
     mvprintw(BOARD_SIZE, 0, "Could not open savefile");
   }
 
-  fscanf(game, "%i", &(board->tries));
+  fscanf(tries, "%i", &(board->tries));
+
+  while(fscanf(game, "%i", tile) != EOF){
+    tile++;
+  }
 
   fclose(game);
+  fclose(tries);
 
   //mvprintw(BOARD_SIZE + 3, 0, "Game loaded!");
 }
